da0698f1020fde191c0b858fbb3336dd32732e73
Kimzegal <135003325+Kimzegal@users.noreply.github.com>
2023-09-10 23:29:14 +0900

    [Silver III] Title: N과 M (3), Time: 328 ms, Memory: 111780 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.java
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -1,6 +1,5 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.StringTokenizer;
+import java.io.*;
 
 public class Main {
 	
@@ -10,6 +9,7 @@ public class Main {
 	
 	public static void main(String args[]) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
@@ -17,7 +17,8 @@ public class Main {
 		
 		combination(0);
 		
-		System.out.println(sb.toString());
+		bw.write(sb.toString());
+		bw.close();
 
 	}
 	
