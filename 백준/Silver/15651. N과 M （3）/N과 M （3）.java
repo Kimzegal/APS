8035299597f630f22b970d294b2abdc8f681de66
Kimzegal <135003325+Kimzegal@users.noreply.github.com>
2023-09-10 23:27:48 +0900

    [Silver III] Title: N과 M (3), Time: 332 ms, Memory: 111672 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	public static int N, M;
+	public static int[] answer;
+	public static StringBuilder sb = new StringBuilder();
+	
+	public static void main(String args[]) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		answer = new int[M];
+		
+		combination(0);
+		
+		System.out.println(sb.toString());
+
+	}
+	
+	public static void combination(int depth) {
+		if(depth == M) {
+			for(int num : answer) {
+				sb.append(num).append(' ');
+			}
+			sb.append('\n');
+		}
+		
+		else {
+			for(int i = 0; i < N; i++) {
+				answer[depth] = i+1;
+				combination(depth+1);
+			}
+		}
+	}
+}
