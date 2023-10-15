08a43c7c5e4ce8fe7227970816eb8f500c5de1ea
Kimzegal <asdskylee@naver.com>
2023-10-16 01:37:20 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 452 ms, Memory: 118328 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -3,23 +3,38 @@ import java.util.*;
 
 public class Main{
 	public static void main(String[]args)throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringBuilder sb = new StringBuilder();
-		int N = Integer.parseInt(br.readLine());
+		int N = read();
 		int[] cards = new int[20_000_001];
-		StringTokenizer st = new StringTokenizer(br.readLine());
 		for(int i = 0; i < N; i++) {
-			cards[Integer.parseInt(st.nextToken())+10_000_000]++;
+			cards[read()+10_000_000]++;
 		}
 
-		int M = Integer.parseInt(br.readLine());
-		st = new StringTokenizer(br.readLine());
+		int M = read();
 		for(int i = 0 ; i < M; i++) {
-			sb.append(cards[Integer.parseInt(st.nextToken())+10_000_000]).append(' ');
+			sb.append(cards[read()+10_000_000]).append(' ');
 		}
 		
 		bw.write(sb.toString());
 		bw.close();
 	}
+        static int read() throws IOException{
+    	int n = 0;
+    	boolean isNumber = false;
+    	boolean isNegative = false;
+    	while(true) {
+    		int input = System.in.read();
+    		if(input<=32) {
+    			if(!isNumber) continue;
+    			else return isNegative?n*(-1):n;
+    		}
+    		
+    		else if(input=='-') isNegative = true;
+    		else {
+    			isNumber = true;
+    			n = (n<<3)+(n<<1)+(input&15);
+    		}
+    	}
+    }
 }
