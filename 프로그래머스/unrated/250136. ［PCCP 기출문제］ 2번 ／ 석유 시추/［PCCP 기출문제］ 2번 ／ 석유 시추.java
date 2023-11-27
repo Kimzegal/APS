dc4589f382fd4d8e47059cee05ebd982f3930c59
Kimzegal <asdskylee@naver.com>
2023-11-28 00:47:42 +0900

    [level unrated] Title: [PCCP 기출문제] 2번 / 석유 시추, Time: 34.26 ms, Memory: 64.5 MB -BaekjoonHub

--- 프로그래머스/unrated/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.java
+++ 프로그래머스/unrated/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.java
@@ -4,18 +4,14 @@ class Solution {
     // 방향 벡터 선언
     public static int[] dr = {-1, 0, 1, 0};
     public static int[] dc = {0, 1, 0, -1};
-    // 마크 배열 : 해당 석유가 '몇번째 석유 덩어리인지' 체크하기 위한 배열
-    public static int[][] mark;
-    // 마크 넘버 : 1부터 시작(0번은 석유가 아닌 곳)하여, 매 석유 덩어리를 찾을 때마다 늘어남
-    public static int mark_num = 1;
-    // map : 덩어리 번호와 매장량을 key-value로 저장하기 위함
-    public static Map<Integer, Integer> map = new HashMap<>();
+    // 마크 배열 : 해당 열에 '얼마나 석유를 캘수 있는지' 체크하기 위한 배열
+    public static int[] mark;
     
 
     public int solution(int[][] land) {
         int answer = 0;
-        // mark를 land와 같은 크기의 배열을 선언
-        mark = new int[land.length][land[0].length];
+        // mark를 land의 열과 같은 크기의 배열을 선언
+        mark = new int[land[0].length];
         // 전체적으로 bfs를 먼저 돌리기 위한 방문 배열 선언
         boolean[][] visited = new boolean[land.length][land[0].length];
         for(int i = 0; i < land.length; i++){
@@ -24,23 +20,10 @@ class Solution {
                 bfs(visited, land, i, j);
             }
         }
-        // bfs가 끝나면 mark엔 각 덩어리 번호가 입력되어 있다.
-        // 따라서, 다시 열따라 반복문을 돌려서 최고 매장량을 찾아야 한다.
-        for(int i = 0; i < land[0].length; i++){
-            // 덩어리의 갯수와 동일한 방문배열 선언 : "그 덩어리를 이번 열에서 이미 시추했는지"
-            boolean[] marked = new boolean[mark_num+1]; // +1해줘서 코드를 깔끔하게.
-            // 이번 열에서의 시추량
-            int sum = 0;
-            for(int j = 0; j < land.length; j++){
-                // 석유 덩어리가 매장되어 있고, 아직 그 덩어리를 방문한 적 없다면
-                if(mark[j][i] != 0 && !marked[mark[j][i]]){
-                    // 방문체크하고, 그 덩어리에 해당하는 매장량을 sum에 더해준다.
-                    marked[mark[j][i]] = true;
-                    sum += map.get(mark[j][i]);
-                }
-            }
+        // bfs가 끝나면 mark엔 열별 최고 매장량이 들어가 있따.
+        for(int i = 0; i < mark.length; i++){
             // answer은 그중 최고값
-            answer = Math.max(sum, answer);
+            answer = Math.max(mark[i], answer);
         }
         
         return answer;
@@ -52,12 +35,14 @@ class Solution {
         if(visited[r][c] || land[r][c] == 0) return;
         // 반환할 석유 매장량을 선언한다. 석유가 있음을 전제로 하므로 1부터 시작.
         int total = 1;
+        // 이번 bfs에서 덩어리가 차지하는 열의 정보를 담을 Set 선언
+        Set<Integer> set = new HashSet<>();
         // 좌표 정보를 담을 큐를 선언
         Queue<int[]> queue = new LinkedList<>();
         // 시작점을 방문처리하고 큐에 넣는다.
         int[] start = new int[] {r, c};
         visited[r][c] = true;
-        mark[r][c] = mark_num;
+        set.add(c);
         queue.offer(start);
         
         while(!queue.isEmpty()){
@@ -71,17 +56,19 @@ class Solution {
                 if(!visited[cur[0]+dr[i]][cur[1]+dc[i]] && land[cur[0]+dr[i]][cur[1]+dc[i]] != 0){
                     // 총 매장량을 더해주고, 방문처리를 해준 뒤 큐에 넣어준다.
                     total++;
+                    set.add(cur[1]+dc[i]);
                     visited[cur[0]+dr[i]][cur[1]+dc[i]] = true;
                     int[] now = new int[] {cur[0]+dr[i], cur[1]+dc[i]};
                     queue.offer(now);
                     
                     // mark에 덩어리 번호를 표시해준다.
-                    mark[cur[0]+dr[i]][cur[1]+dc[i]] = mark_num;
                 }
             }
         }
-        // 모든게 끝났으면 이번 덩어리에 해당하는 매장량을 map에 넣어주고, 다음 덩어리를 체크한다.
-        map.put(mark_num++, total);
+        Iterator<Integer> iter = set.iterator(); // set을 Iterator 안에 담기
+		while(iter.hasNext()) { // iterator에 다음 값이 있다면
+			mark[iter.next()] += total;
+		}
         
     }
     
