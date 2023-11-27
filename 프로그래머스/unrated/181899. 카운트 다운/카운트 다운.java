71257f486ed6e0a580fd2aa0c56629b0c294ec72
Kimzegal <asdskylee@naver.com>
2023-11-27 23:19:11 +0900

    [level unrated] Title: 카운트 다운, Time: 0.04 ms, Memory: 67.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181899. 카운트 다운/카운트 다운.java
+++ 프로그래머스/unrated/181899. 카운트 다운/카운트 다운.java
@@ -1,9 +1,11 @@
+import java.util.*;
+
 class Solution {
-    public int[] solution(int start_num, int end_num) {
-        int[] answer = new int[start_num - end_num + 1];
+    public List<Integer> solution(int start_num, int end_num) {
+        List<Integer> answer = new ArrayList<>();
         
-        for(int i = 0; i < answer.length; i++){
-            answer[i] = start_num--;    
+        for(int i = start_num; i >= end_num; i--){
+            answer.add(i);
         }
         
         return answer;
