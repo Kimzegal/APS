634a3d56f06dacdc328f5a9ad897425d24d008cd
Kimzegal <asdskylee@naver.com>
2023-12-28 17:11:18 +0900

    [Silver III] Title: 안테나, Time: 960 ms, Memory: 164648 KB -BaekjoonHub

--- 백준/Silver/18310. 안테나/안테나.java
+++ 백준/Silver/18310. 안테나/안테나.java
@@ -1,26 +1,19 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int N = Integer.parseInt(br.readLine());
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int[] house = new int[100001];
-		
-		for(int i=0; i<N; i++) {
-			house[Integer.parseInt(st.nextToken())] += 1;
-		}
-		
-		int cnt = (N-1)/2;
-		for(int i=1; i<=100000; i++) {
-			if(house[i]!=0) {
-				cnt-=house[i];
-				if(cnt<0) {System.out.println(i); break;}
-			}
-		}
-	}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt(); // 집의 갯수
+        int[] arr = new int[N]; // 집의 위치
+        int ans = 0;
+
+        for(int i = 0; i < N; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        Arrays.sort(arr); // 정렬
+
+        System.out.println(arr[(N-1)/2]); // 중앙값 출력
+    }
 }
