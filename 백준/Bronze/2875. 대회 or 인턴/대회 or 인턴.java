53be9d906ef65d3651b8c3fce9470126fc44221f
Kimzegal <asdskylee@naver.com>
2024-01-09 15:35:43 +0900

    [Bronze III] Title: 대회 or 인턴, Time: 72 ms, Memory: 11412 KB -BaekjoonHub

--- 백준/Bronze/2875. 대회 or 인턴/대회 or 인턴.java
+++ 백준/Bronze/2875. 대회 or 인턴/대회 or 인턴.java
@@ -1,38 +1,37 @@
-import java.io.*;
-import java.util.*;
-
+/*
+ * 첫번째 아이디어 : 냅다 반복문을 돌린다.
+ */
 public class Main {
 
-	public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
 
-		int N = read();
-		int M = read();
-		int K = read();
-		int max = 0;
-		
-		for(int i=0; i<=K; i++) {
-			max = Math.max(Math.min((N-i)/2, M-(K-i)),max);
-		}
+        int N = read();
+        int M = read();
+        int K = read();
 
-		System.out.println(max);
-	}
-    
-    static int read() throws IOException{
-    	int n = 0;
-    	boolean isNumber = false;
-    	boolean isNegative = false;
-    	while(true) {
-    		int input = System.in.read();
-    		if(input<=32) {
-    			if(!isNumber) continue;
-    			else return isNegative?n*(-1):n;
-    		}
-    		
-    		else if(input=='-') isNegative = true;
-    		else {
-    			isNumber = true;
-    			n = (n<<3)+(n<<1)+(input&15);
-    		}
-    	}
+        int max = 0;
+
+        for (int i = 0; i <= K; i++) {
+            max = Math.max(max, Math.min((N-i)/2, (M-K+i)));
+        }
+
+        System.out.println(max);
+    }
+    public static int read() throws Exception{
+        int n = 0;
+        int cur;
+        boolean isNumber = false;
+        while(true){
+            cur = System.in.read();
+            if(cur <= 32){
+                if(isNumber){
+                    return n;
+                }
+            }
+            else{
+                isNumber = true;
+                n = (n<<3) + (n<<1) + (cur&15);
+            }
+        }
     }
 }
