5e5f4efdeac31a74099224a317723edae7cf415a
Kimzegal <asdskylee@naver.com>
2024-01-10 14:24:20 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 88 ms, Memory: 12048 KB -BaekjoonHub

--- 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.java
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.java
@@ -1,44 +1,49 @@
-	import java.io.*;
-	import java.util.*;
-	
-	public class Main {
-	
-		public static void main(String[] args) throws IOException {
-			
-			int a = read();
-			int b = read();
-			int c = read();
-			int d = read();
-			int e = read();
-			int f = read();
-			
-			label:
-			for(int i=-999; i<=999; i++) {
-				for(int j=-999; j<=999; j++) {
-					if((a*i + b*j == c) && (d*i + e*j == f)) {
-						System.out.println(i+" "+j);
-                        return;
-					}
-				}
-			}
-		}
+/*
+ * 첫번째 아이디어 : 냅다 반복문을 돌린다.
+ */
+public class Main {
 
-        static int read() throws IOException{
-    	int n = 0;
-    	boolean isNumber = false;
-    	boolean isNegative = false;
-    	while(true) {
-    		int input = System.in.read();
-    		if(input<=32) {
-    			if(!isNumber) continue;
-    			else return isNegative?n*(-1):n;
-    		}
-    		
-    		else if(input=='-') isNegative = true;
-    		else {
-    			isNumber = true;
-    			n = (n<<3)+(n<<1)+(input&15);
-    		}
-    	}
+    public static void main(String[] args) throws Exception {
+
+        /*
+         입력받기
+         */
+        int a = read();
+        int b = read();
+        int c = read();
+        int d = read();
+        int e = read();
+        int f = read();
+
+        for(int i = -999; i <= 999; i++){
+            for(int j = -999; j <= 999; j++){
+                if(a*i + b*j == c && d*i + e*j == f){
+                    System.out.print(i+" "+j);
+                    return;
+                }
+            }
+        }
     }
-	}
+    public static int read() throws Exception{
+        int n = 0;
+        int cur;
+        boolean isNumber = false;
+        boolean isNegative = false;
+        while(true){
+            cur = System.in.read();
+            if(cur == '-'){
+                isNegative = true;
+            }
+            else if(cur <= 32){
+                if(isNumber){
+                    int now = isNegative ? -n : n;
+                    return now;
+                }
+            }
+            else{
+                isNumber = true;
+                n = (n<<3) + (n<<1) + (cur&15);
+            }
+        }
+    }
+}
