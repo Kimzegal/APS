54bd92d23c57fc6c0e72472987a228c58dd5008d
Kimzegal <asdskylee@naver.com>
2024-02-18 17:29:46 +0900

    [Silver III] Title: N과 M (3), Time: 372 ms, Memory: 111740 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.java
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -1,40 +1,61 @@
-import java.util.StringTokenizer;
+import java.util.*;
 import java.io.*;
 
+/*
+    기본적인 백트래킹
+ */
 public class Main {
-	
-	public static int N, M;
-	public static int[] answer;
-	public static StringBuilder sb = new StringBuilder();
-	
-	public static void main(String args[]) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		answer = new int[M];
-		
-		combination(0);
-		
-		bw.write(sb.toString());
-		bw.close();
+    public static int N,M;
+    public static StringBuilder sb = new StringBuilder();
+    public static int[] arr;
+    public static void main(String[] args) throws Exception {
+        N = read(); // 자연수
+        M = read(); // 중복없이 고르는 수
+        arr = new int[M]; // 수 저장
 
-	}
-	
-	public static void combination(int depth) {
-		if(depth == M) {
-			for(int num : answer) {
-				sb.append(num).append(' ');
-			}
-			sb.append('\n');
-		}
-		
-		else {
-			for(int i = 0; i < N; i++) {
-				answer[depth] = i+1;
-				combination(depth+1);
-			}
-		}
-	}
+        permutation(0);
+
+        System.out.println(sb);
+    }
+
+    public static void permutation(int depth){
+        if(depth == M){
+            for(int i = 0; i < M; i++){
+                sb.append(arr[i]).append(" ");
+            }
+            sb.append("\n"); // 개행
+            return;
+        }
+
+        else{
+            for(int i = 1; i <= N; i++){
+                arr[depth] = i;
+                permutation(depth + 1);
+            }
+        }
+    }
+
+
+    public static int read() throws Exception{
+        int n = 0;
+        int cur;
+        boolean isNumber = false;
+        boolean isNegative = false;
+        while(true){
+            cur = System.in.read();
+            if(cur == '-'){
+                isNegative = true;
+            }
+            else if(cur <= 32){
+                if(isNumber){
+                    return isNegative ? -n : n;
+                }
+            }
+            else{
+                isNumber = true;
+                n = (n<<3) + (n<<1) + (cur&15);
+            }
+        }
+    }
+
 }
